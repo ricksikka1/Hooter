8861454df29bccd2f81c75445e22ac4866ea8b16
rick <ricksikka1@gmail.com>
2020-10-08 20:59:07 -0400

    loading gid

--- client/client.js
+++ client/client.js
@@ -1,1 +1,23 @@
 console.log("hello world");
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const hoot = {
+        name,
+        content
+    };
+
+    loadingElement.style.display = '';
+    form.style.display = 'none';
+
+    console.log(hoot);
+});
